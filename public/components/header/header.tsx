2b2167e55745280bfbda6be52c9e8c3c9fb68054
FedaJakic <fedja.jakic@gmail.com>
2021-10-20 23:48:26 +0200

    setup project

---
+++ public/components/header/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+interface HeaderProps {
+    
+}
+ 
+interface HeaderState {
+    
+}
+ 
+class Header extends React.Component<HeaderProps, HeaderState> {
+    constructor(props: HeaderProps) {
+        super(props);
+        // this.state = { :  };
+    }
+    render() { 
+        return ( null );
+    }
+}
+ 
+export default Header;
